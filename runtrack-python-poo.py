c12ebb0100e798774f22d2e6cc8efb3039525a32
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-02 09:48:11 +0100

    job 01

---
+++ runtrack-python-poo.py
@@ -0,0 +1,10 @@
+class Operation:
+    def __init__(self, nombre1=0, nombre2=0):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+
+# Instanciation de la classe
+operation_instance = Operation()
+
+# Résultat de l'objet en console
+print(operation_instance)
