9b6b6eb467e13f5483d9165a0e1777ff591775fd
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-02 14:41:32 +0100

    add update job06

--- Jour01/job06.py
+++ Jour01/job06.py
@@ -10,12 +10,28 @@ class Animal:
     def nommer(self, nom):
         self.prenom = nom
 
+# Fonction pour saisir l'âge initial et le nom de l'animal
+def saisir_infos_animal():
+    while True:
+        try:
+            age_initial = int(input("Veuillez entrer l'âge initial de l'animal : "))
+            break
+        except ValueError:
+            print("Veuillez entrer un nombre entier pour l'âge.")
+
+    nom_animal = input("Veuillez entrer le nom de l'animal : ")
+    return age_initial, nom_animal
+
+initial_age, animal_name = saisir_infos_animal()
+
 mon_animal = Animal()
+
+mon_animal.age = initial_age
+mon_animal.nommer(animal_name)
 
 print("Âge initial de l'animal :", mon_animal.age)
 
 mon_animal.vieillir()
 print("Âge de l'animal après vieillissement :", mon_animal.age)
 
-mon_animal.nommer("Fido")
 print("Nom de l'animal :", mon_animal.prenom)
