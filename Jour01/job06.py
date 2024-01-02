e78763b5528aa38c9e39568787940ff19c552054
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-02 15:07:57 +0100

    add update job06

--- Jour01/job06.py
+++ Jour01/job06.py
@@ -1,37 +1,18 @@
-class Animal:
+class Animal():
+    def __init__(self, prenom="", age = 0):
+        self.prenom = prenom
+        self.age = age
+
+    def veillir(self):
+        self.age = input("Quel est l'age de votre animal :")
+        self.age =+1
+        print("Animal a", self.age)
     
-    def __init__(self):
-        self.age = 0
-        self.prenom = ""
+    def nommer(self):
+        self.prenom = input("Quel est le nom de votre animal :")
+        print("Le nom de l'animal est", self.prenom)
 
-    def vieillir(self):
-        self.age += 1
+patte = Animal()
 
-    def nommer(self, nom):
-        self.prenom = nom
-
-# Fonction pour saisir l'âge initial et le nom de l'animal
-def saisir_infos_animal():
-    while True:
-        try:
-            age_initial = int(input("Veuillez entrer l'âge initial de l'animal : "))
-            break
-        except ValueError:
-            print("Veuillez entrer un nombre entier pour l'âge.")
-
-    nom_animal = input("Veuillez entrer le nom de l'animal : ")
-    return age_initial, nom_animal
-
-initial_age, animal_name = saisir_infos_animal()
-
-mon_animal = Animal()
-
-mon_animal.age = initial_age
-mon_animal.nommer(animal_name)
-
-print("Âge initial de l'animal :", mon_animal.age)
-
-mon_animal.vieillir()
-print("Âge de l'animal après vieillissement :", mon_animal.age)
-
-print("Nom de l'animal :", mon_animal.prenom)
+patte.veillir()
+patte.nommer()
