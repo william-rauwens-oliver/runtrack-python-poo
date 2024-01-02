9a97a8acb1c6defd3c042c43b01d1a72b7b2698b
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-02 10:15:39 +0100

    update job01

---
+++ Jour01/job03.py
@@ -0,0 +1,11 @@
+class Operation:
+    def __init__(self, nombre1, nombre2):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+
+    def addition(self):
+        resultat = self.nombre1 + self.nombre2
+        print(f"Résultat de l'addition : {resultat}")
+
+operation_instance = Operation(5, 7)
+operation_instance.addition()
