758d7ac3b76cd2784f7ebbda815e6041c12626ba
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-03 12:04:40 +0100

    add update credits

--- Jour02/job04.py
+++ Jour02/job04.py
@@ -4,13 +4,14 @@ class Student:
         self.__prenom = prenom
         self.__numero_de_etudiant = numero_de_etudiant
         self.__credits = credits
-        self.__level = self._student_eval()
+        self.__level = self.__student_eval()
 
     def add_credits(self, credits):
         if credits > 0:
             self.__credits += credits
+            self.__level = self.__student_eval()
         else:
-            print("Erreur : Le nombre de crédits doit etre supérieur à zéro")
+            print("Erreur : Le nombre de crédits doit être supérieur à zéro")
 
     def __student_eval(self):
         if self.__credits >= 90:
@@ -27,8 +28,8 @@ class Student:
     def student_info(self):
         print(f"Nom: {self.__nom}")
         print(f"Prénom: {self.__prenom}")
-        print(f"Numéro d'étudiant: {self.__numero_etudiant}")
-        print(f"Niveau:{self.__level}")
+        print(f"Numéro d'étudiant: {self.__numero_de_etudiant}")
+        print(f"Niveau: {self.__level}")
 
 john_doe = Student("Doe", "John", 145)
 john_doe.add_credits(30)
