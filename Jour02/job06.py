64ff7678c6d03b5987657a677ddabde2785bf8e9
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-03 15:31:07 +0100

    add update ajouter plat

--- Jour02/job06.py
+++ Jour02/job06.py
@@ -6,3 +6,12 @@ class Commande:
 
     def ajouter_plat(self, nom_plat, prix_plat):
         self.__plats_commandes[nom_plat] = {"prix": prix_plat, "statut": self.__statut_commande}
+
+    def annuler_commande(self):
+        self.__statut_commande = "annulé"
+        for plat in self.__plats_commandes:
+            self.__plats_commandes[plat]["statut"] = self.__statut_commande
+
+    def __calculer_total(self):
+        total = sum(plat["prix"] for plat in self.__plats_commandes.values())
+        return total
