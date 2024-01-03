dcadad95b9c7922cc110a18235996172e36d5724
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-03 16:13:59 +0100

    add update ajouter plat

--- Jour02/job06.py
+++ Jour02/job06.py
@@ -18,8 +18,23 @@ class Commande:
 
     def afficher_commande(self):
         print(f"Commande #{self.__numero_commande}:")
-        for plat, details in self.__plat_commandes.items():
-            print(f"{plat}: {details['prix']} € - Statut: {details['status']}")
-            total = self.__calculer_total()
-            print(f"Total à payer: {total} €")
-    
+        for plat, details in self.__plats_commandes.items():  # Correction ici
+            print(f"{plat}: {details['prix']} € - Statut: {details['statut']}")
+        total = self.__calculer_total()
+        print(f"Total à payer: {total} €")
+
+    def calculer_tva(self, taux_tva):
+        total = self.__calculer_total()
+        tva = total * (taux_tva / 100)
+        return tva
+
+commande1 = Commande(1)
+commande1.ajouter_plat("Lasagne", 9.99)
+commande1.ajouter_plat("Coulant au chocolat", 4.99)
+commande1.afficher_commande()
+
+tva_calculee = commande1.calculer_tva(5)
+print(f"TVA calculée : {tva_calculee} €")
+
+commande1.annuler_commande()
+commande1.afficher_commande()
