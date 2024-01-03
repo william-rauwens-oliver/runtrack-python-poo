518bba0b7c9b05edf97aac92a0eaec7b3ee8bc5d
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-03 13:03:13 +0100

    add job05 voiture

---
+++ Jour02/job05.py
@@ -0,0 +1,8 @@
+class Voiture:
+    def __init__(self, marque, modele, annee, kilometrage, en_marche=False, reservoir=50):
+        self.__marque = marque
+        self.__modele = modele
+        self.__annee = annee
+        self.__kilometrage = kilometrage
+        self.__en_marche = en_marche
+        self.__reservoir = reservoir
