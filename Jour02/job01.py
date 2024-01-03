1ca7dd872b8dc18556291460f053677cdb8b0dfe
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-03 11:01:46 +0100

    add update Mutateurs

--- Jour02/job01.py
+++ Jour02/job01.py
@@ -1,24 +1,31 @@
 class Rectangle:
     def __init__(self, longueur, largeur):
-        self.longueur = longueur
-        self.largeur = largeur
+        self.__longueur = longueur
+        self.__largeur = largeur
 
+# Assesseurs (getters)
+        
     def get_longueur(self):
-        return self.longueur
+        return self.__longueur
+
     def get_largeur(self):
-        return self.largeur
+        return self.__largeur
+
+# Mutateurs (setters)
+    
     def set_longueur(self, nouvelle_longueur):
-        self.longueur = nouvelle_longueur
+        self.__longueur = nouvelle_longueur
+
     def set_largeur(self, nouvelle_largeur):
-        self.largeur = nouvelle_largeur
+        self.__largeur = nouvelle_largeur
 
 mon_rectangle = Rectangle(10, 5)
 
-print("Longueur initiale: ", mon_rectangle.get_longueur())
-print("Largeur initiale: ", mon_rectangle.get_largeur())
+print("Longueur initiale:", mon_rectangle.get_longueur())
+print("Largeur initiale:", mon_rectangle.get_largeur())
 
 mon_rectangle.set_longueur(15)
 mon_rectangle.set_largeur(8)
 
-print("Nouvelle longueur: ", mon_rectangle.get_longueur())
-print("Nouvelle Largeur: ", mon_rectangle.get_largeur())
+print("Nouvelle longueur:", mon_rectangle.get_longueur())
+print("Nouvelle largeur:", mon_rectangle.get_largeur())
