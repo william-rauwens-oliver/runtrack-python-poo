4455462ae5adfcf6dbfe60680ab8e9763e67cc81
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-03 10:48:22 +0100

    add Mutateurs

--- Jour02/job03.py
+++ Jour02/job03.py
@@ -7,14 +7,29 @@ class Livre:
 
 # Assesseurs
 
-def get_titre(self):
-    return self.__titre
+    def get_titre(self):
+        return self.__titre
 
-def get_auteur(self):
-    return self.__auteur
+    def get_auteur(self):
+        return self.__auteur
 
-def get_nombre_de_pages(self):
-    return self.__nombre_de_pages
+    def get_nombre_de_pages(self):
+        return self.__nombre_de_pages
 
-def est_disponible(self):
-    return self.__disponible
+    def est_disponible(self):
+        return self.__disponible
+
+# Mutateurs
+
+    def set_titre(self, nouveau_titre):
+        self.__titre = nouveau_titre
+
+    def set_auteur(self, nouvel_auteur):
+        self.__auteur = nouvel_auteur
+
+    def set_nombre_de_pages(self, nouveau_nombre_de_pages):
+        if isinstance(nouveau_nombre_de_pages, int) and nouveau_nombre_de_pages > 0:
+            self.__nombre_de_pages = nouveau_nombre_de_pages
+        else:
+            print("Erreur : Le nombre de pages demandé doit être un nombre entier positif !")
+
