65afecba23b3e5606be2e8e97f1c69eff65aca31
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-03 13:18:56 +0100

    add job05 voiture

--- Jour02/job04.py
+++ Jour02/job04.py
@@ -4,16 +4,16 @@ class Student:
         self.__prenom = prenom
         self.__numero_de_etudiant = numero_de_etudiant
         self.__credits = credits
-        self.__level = self.__student_eval()
+        self.__level = self.__etudiant_eval()
 
     def add_credits(self, credits):
         if credits > 0:
             self.__credits += credits
-            self.__level = self.__student_eval()
+            self.__level = self.__etudiant_eval()
         else:
             print("Erreur : Le nombre de crédits doit être supérieur à zéro")
 
-    def __student_eval(self):
+    def __etudiant_eval(self):
         if self.__credits >= 90:
             return "Excellent"
         elif self.__credits >= 80:
@@ -22,17 +22,18 @@ class Student:
             return "Bien"
         elif self.__credits >= 60:
             return "Passable"
-        else: 
+        else:
             return "Insuffisant"
-        
-    def student_info(self):
+
+    def etudiant_info(self):
         print(f"Nom: {self.__nom}")
         print(f"Prénom: {self.__prenom}")
         print(f"Numéro d'étudiant: {self.__numero_de_etudiant}")
+        print(f"Le nombre de crédits de {self.__prenom} {self.__nom} est de {self.__credits} points.")
         print(f"Niveau: {self.__level}")
 
 john_doe = Student("Doe", "John", 145)
 john_doe.add_credits(30)
 john_doe.add_credits(25)
 john_doe.add_credits(20)
-john_doe.student_info()
+john_doe.etudiant_info()
--- Jour02/job05.py
+++ Jour02/job05.py
@@ -6,3 +6,38 @@ class Voiture:
         self.__kilometrage = kilometrage
         self.__en_marche = en_marche
         self.__reservoir = reservoir
+
+# Mutateurs (setters) et assesseurs (getters) pour chaque attribut
+
+    def get_marque(self):
+        return self.__marque
+
+    def set_marque(self, nouvelle_marque):
+        self.__marque = nouvelle_marque
+
+    def get_modele(self):
+        return self.__modele
+
+    def set_modele(self, nouveau_modele):
+        self.__modele = nouveau_modele
+
+    def get_annee(self):
+        return self.__annee
+
+    def set_annee(self, nouvelle_annee):
+        self.__annee = nouvelle_annee
+
+    def get_kilometrage(self):
+        return self.__kilometrage
+
+    def set_kilometrage(self, nouveau_kilometrage):
+        self.__kilometrage = nouveau_kilometrage
+
+    def get_en_marche(self):
+        return self.__en_marche
+
+    def set_en_marche(self, en_marche):
+        self.__en_marche = en_marche
+
+    def get_reservoir(self):
+        return self.__reservoir
