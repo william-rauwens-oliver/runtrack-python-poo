ffd989e74972b24f696e0385542668177b642720
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-03 10:42:57 +0100

    add assesseurs

--- Jour02/job03.py
+++ Jour02/job03.py
@@ -1,6 +1,20 @@
 class Livre:
-    def __init__(self, titre, auteur, __nombre_de_pages):
+    def __init__(self, titre, auteur, nombre_de_pages):
         self.__titre = titre
         self.__auteur = auteur
         self.__nombre_de_pages = nombre_de_pages
         self.__disponible = True
+
+# Assesseurs
+
+def get_titre(self):
+    return self.__titre
+
+def get_auteur(self):
+    return self.__auteur
+
+def get_nombre_de_pages(self):
+    return self.__nombre_de_pages
+
+def est_disponible(self):
+    return self.__disponible
