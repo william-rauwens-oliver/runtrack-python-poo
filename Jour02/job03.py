8d6489772b87f0a3e41360fae45da490dd1b6d2f
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-03 10:28:22 +0100

    add job03

---
+++ Jour02/job03.py
@@ -0,0 +1,6 @@
+class Livre:
+    def __init__(self, titre, auteur, __nombre_de_pages):
+        self.__titre = titre
+        self.__auteur = auteur
+        self.__nombre_de_pages = nombre_de_pages
+        self.__disponible = True
