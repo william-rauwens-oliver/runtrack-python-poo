8f19e46ec282978147fdc8b79162edba3e4712fb
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-04 12:00:33 +0100

    add class and def ajouter tache and ListeDestaches

---
+++ jour03/job03.py
@@ -0,0 +1,13 @@
+class Tache:
+    def __init__(self, titre, description, statut="à faire"):
+        self.titre = titre
+        self.description = description
+        self.statut = statut
+
+class ListeDeTaches:
+    def __init__(self):
+        self.taches = []
+
+    def ajouterTache(self, tache):
+        self.taches.append(tache)
+        print(f"Tâche ajoutée : {tache.titre}")
