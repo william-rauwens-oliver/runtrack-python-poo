1d121edb369242d3f844ce499b42b9d58679d26f
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-04 16:20:45 +0100

    add Personnage

--- jour03/job05.py
+++ jour03/job05.py
@@ -4,3 +4,6 @@ class Personnage:
     def __init__(self, nom, vie):
         self.nom = nom
         self.vie = vie
+    def attaquer(self, adversaire, degats):
+        print(f"{self.nom} attaque {adversaire.nom} et lui inflige {degats} points de dégats.")
+        adversaire.vie -= degats
