5ff7c696b36e12272015386f1eb97a827bf04f88
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-04 10:06:04 +0100

    add fix bug

--- jour03/job01.py
+++ jour03/job01.py
@@ -1,28 +1,31 @@
 class Ville:
-    def __init__(self, prenom, nombre_habitants):
-        self.prenom = prenom
-        self.nombre_habitants = nombre_habitants
+    def __init__(self, nom, nombre_habitants):
+        self.nom = nom
+        self._nombre_habitants = nombre_habitants
     def get_nombre_habitants(self):
         return self._nombre_habitants
     def ajouter_population(self):
-        self.get_nombre_habitants +=1
+        self._nombre_habitants += 1
 
 class Personne:
     def __init__(self, prenom, age, ville):
-            self._prenom = prenom
-            self._age = age
-            self.ville = ville
-            ville.ajouter_population()
-
+        self._prenom = prenom
+        self._age = age
+        self._ville = ville
+        ville.ajouter_population()
+        
     def ajouter_population(self):
-         self._ville.ajouter_population()
+        self._ville.ajouter_population()
 
 paris = Ville("Paris", 1000000)
-print(f"Nombre d'habitants à Paris : {paris.get_nombre_habitants}")
+print(f"Nombre d'habitants à Paris : {paris.get_nombre_habitants()}")
 
 marseille = Ville("Marseille", 861635)
-print(f"Nombre d'habitant à Marseille : {marseille.get_nombre_habitants}",)
+print(f"Nombre d'habitants à Marseille : {marseille.get_nombre_habitants()}")
 
 john = Personne("John", 45, paris)
 myrtille = Personne("Myrtille", 4, paris)
 chloe = Personne("Chloé", 18, marseille)
+
+print(f"Nombre d'habitants à Paris à l'arrivée de John et Myrtille : {paris.get_nombre_habitants()}")
+print(f"Nombre d'habitants à Marseille à l'arrivée de Chloé : {marseille.get_nombre_habitants()}")
