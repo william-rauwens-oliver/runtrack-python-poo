2e45cdadf36d16a279562f2941445ba78cfc88c3
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-04 14:17:24 +0100

    add def carton

---
+++ jour03/job04.py
@@ -0,0 +1,9 @@
+class Joueur:
+    def __init__(self, nom, numero, position):
+        self.nom = nom
+        self.numero = numero
+        self.position = position
+        self.buts_marques = 0
+        self.passes_decisives = 0
+        self.cartons_jaunes = 0
+        self.cartons_rouges = 0
