a2e7dde548b67b2fb200db15de97b363afc0d569
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-04 14:43:01 +0100

    add class equipe

--- jour03/job04.py
+++ jour03/job04.py
@@ -20,3 +20,21 @@ class Joueur:
     def recevoirUnCartonRouge(self):
         self.cartons_rouges += 1
 
+class Equipe:
+    def __init__(self, nom):
+        self.nom = nom
+        self.joueurs = []
+
+    def ajouterJoueur(self, joueur):
+        self.joueurs.append(joueur)
+
+    def afficherStatistiquesJoueurs(self):
+        print(f"Statistiques de l'équipe {self.nom}:")
+        for joueur in self.joueurs:
+            print(f"{joueur.nom} - Buts: {joueur.buts_marques}, Passes décisives: {joueur.passes_decisives}, Cartons jaunes: {joueur.cartons_jaunes}, Cartons rouges: {joueur.cartons_rouges}")
+
+    def mettreAJourStatistiquesJoueur(self, joueur, buts=0, passes_decisives=0, cartons_jaunes=0, cartons_rouges=0):
+        joueur.buts_marques += buts
+        joueur.passes_decisives += passes_decisives
+        joueur.cartons_jaunes += cartons_jaunes
+        joueur.cartons_rouges += cartons_rouges
