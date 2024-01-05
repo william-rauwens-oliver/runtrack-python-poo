cb9fa09ef571c9569180ae7d50723318f9a2035a
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-05 11:16:58 +0100

    reutilisation de lancienne class

---
+++ jour04/job05.py
@@ -0,0 +1,11 @@
+class Forme:
+    def aire(self):
+        return 0
+
+class Rectangle(Forme):
+    def __init__(self, largeur, hauteur):
+        self.largeur = largeur
+        self.hauteur = hauteur
+
+    def aire(self):
+        return self.largeur * self.hauteur
