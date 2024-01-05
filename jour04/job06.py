fc8353f64311f8427e388fc069c0409e28ce1117
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-05 12:16:42 +0100

    add print

---
+++ jour04/job06.py
@@ -0,0 +1,43 @@
+class Vehicule:
+    def __init__(self, marque, modele, annee, prix):
+        self.marque = marque
+        self.modele = modele
+        self.annee = annee
+        self.prix = prix
+
+    def informationsVehicule(self):
+        print(f"Marque: {self.marque}, Modèle: {self.modele}, Année: {self.annee}, Prix: {self.prix} €")
+
+    def demarrer(self):
+        print("Attention, je roule")
+
+
+class Voiture(Vehicule):
+    def __init__(self, marque, modele, annee, prix, portes=3):
+        super().__init__(marque, modele, annee, prix)
+        self.portes = portes
+
+    def informationsVehicule(self):
+        super().informationsVehicule()
+        print(f"Nombre de portes: {self.portes}")
+
+
+class Moto(Vehicule):
+    def __init__(self, marque, modele, annee, prix, roues=2):
+        super().__init__(marque, modele, annee, prix)
+        self.roues = roues
+
+    def informationsVehicule(self):
+        super().informationsVehicule()
+        print(f"Nombre de roues: {self.roues}")
+
+    def demarrer(self):
+        print("Vroom, vroom ! Prêt à rouler en moto")
+
+
+voiture = Voiture(marque="Mazda", modele="MX-5", annee=2018, prix=33000)
+voiture.informationsVehicule()
+voiture.demarrer()
+moto = Moto(marque="Suzuki", modele="GSX-R1000", annee=2001, prix=20000)
+moto.informationsVehicule()
+moto.demarrer()
