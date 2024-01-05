8e9980917d5bc8273d3fdef40211b303e3a5395f
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-05 10:21:38 +0100

    add clas Personne Eleve

--- jour04/jour02.py
+++ jour04/jour02.py
@@ -10,3 +10,14 @@ class Personne:
 
     def modifierAge(self, nouvel_age):
         self.age = nouvel_age
+
+
+class Eleve(Personne):
+    def __init__(self, age=14):
+        Personne.__init__(self, age)
+
+    def allerEnCours(self):
+        print("Je vais en cours")
+
+    def afficherAge(self):
+        print(f"J'ai {self.age} ans")
