196635dc2c1161b39a30ba2b2c435973da005423
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-05 11:26:59 +0100

    add class cercle

--- jour04/job05.py
+++ jour04/job05.py
@@ -9,3 +9,10 @@ class Rectangle(Forme):
 
     def aire(self):
         return self.largeur * self.hauteur
+    
+class Cercle(Forme):
+    def __init__(self, radius):
+        self.radius = radius
+    def aire(self):
+        return 3.14 * self.radius ** 2
+    
