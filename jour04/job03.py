2be15963eecdadeb47bd0358e5c29f2c278a0e1b
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-05 11:09:03 +0100

    add class parallelepipede

--- jour04/job03.py
+++ jour04/job03.py
@@ -22,3 +22,20 @@ class Rectangle:
         return 2 * (self._longueur + self._largeur)
     def surface(self):
         return self._longueur * self._largeur
+
+class Parallelepipede(Rectangle):
+    def __init__(self, longueur, largeur, hauteur):
+        Rectangle.__init__(self, longueur, largeur)
+        self._hauteur = hauteur
+
+    def get_hauteur(self):
+        return self._hauteur
+    
+    def set_hauteur(self, value):
+        if value > 0:
+            self._hauteur = value
+        else:
+            print("La hauteur doit être positive.")
+
+    def volume(self):
+        return self._longueur * self._largeur * self._hauteur
