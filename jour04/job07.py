01ff33b6bd4ee0b107a18b31e6e8a2cd373b94e8
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-05 13:05:09 +0100

    add carte

--- jour04/job07.py
+++ jour04/job07.py
@@ -4,3 +4,16 @@ class Carte:
     def __init__(self, valeur, couleur):
         self.valeur = valeur
         self.couleur = couleur
+
+class Jeu:
+    def __init__(self):
+        self.paquet = self.creer_paquet()
+        self.main_joueur = []
+        self.main_croupier = []
+
+    def creer_paquet(self):
+        valeurs = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+        couleurs = ['Cœur', 'Carreau', 'Trèfle', 'Pique']
+        paquet = [Carte(valeur, couleur) for valeur in valeurs for couleur in couleurs]
+        random.shuffle(paquet)
+        return paquet
